8cfbf652ddf12d7183219faeba5027523e8e78b1
tidjane95 <tidjane271295@gmail.com>
2020-09-17 18:31:46 +0000

    Add php config, Make Pin entity timetampable listing index by createdAt DESC

--- migrations/Version20200917181622.php
+++ migrations/Version20200917181622.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200917153739 extends AbstractMigration
+final class Version20200917181622 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Create pin table';
+        return '';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE pins (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE pins ADD created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, ADD updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE pins');
+        $this->addSql('ALTER TABLE pins DROP created_at, DROP updated_at');
     }
 }
