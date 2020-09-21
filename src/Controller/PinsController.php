f300fc9c612d6ef060a291ef054881a0ff9ce3d8
tidjane95 <tidjane271295@gmail.com>
2020-09-21 16:00:56 +0000

    Add create/edit form and basic navigation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods="GET")
      */
     public function index(PinRepository $pinRepository): Response
     {
@@ -19,11 +23,55 @@ class PinsController extends AbstractController
         return $this->render('pins/index.html.twig', compact('pins'));
     }
     /**
-     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show", methods="GET")
      */
     public function show(Pin $pin): Response
     {
         return $this->render('pins/show.html.twig', compact('pin'));
+
+    }
+    /**
+     * @Route("/pins/create", name="app_pins_create", methods={"GET", "POST"} )
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $pin= new Pin;
+        $form= $this->createFormBuilder( $pin)
+        -> add('titre', TextType:: class)
+        ->add('description', TextareaType:: class)
+        -> getForm()
+        ;
+        $form->handleRequest($request);
+        if($form-> isSubmitted() && $form-> isValid()){
+            $em -> persist($pin);
+            $em -> flush();
+            return $this-> redirectToRoute('app_home');
+
+        }
+        return $this->render('pins/create.html.twig', [
+            'monForm'=> $form-> createView()
+        ]);
+
+    }
+    /**
+     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "POST"})
+     */
+    public function edit(Pin $pin, Request $request, EntityManagerInterface $em ): Response
+    {
+        $form= $this->createFormBuilder( $pin)
+        -> add('titre', TextType:: class)
+        ->add('description', TextareaType:: class)
+        -> getForm()
+        ;
+        $form->handleRequest($request);
+        if($form-> isSubmitted() && $form-> isValid()){
+            $em -> flush();
+            return $this-> redirectToRoute('app_home');
+
+        }
+        return $this->render('pins/edit.html.twig', [
+            'pin'=> $pin,
+            'monForm'=> $form-> createView()]);
 
     }
 }
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -2,8 +2,9 @@
 
 namespace App\Entity;
 
-use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PinRepository;
+use App\Entity\Traits\Timestampable;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -12,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Pin
 {
+   Use Timestampable; 
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -29,15 +31,7 @@ class Pin
      */
     private $description;
 
-    /**
-     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
-     */
-    private $updatedAt;
+   
 
     public function getId(): ?int
     {
@@ -68,41 +62,6 @@ class Pin
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-    /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     */
-    public function updateTimestamps()
-    {
-        if( $this-> setCreatedAt=== null){
-            $this-> setCreatedAt(new \DateTimeImmutable);
-
-        }
-        
-        $this-> setUpdatedAt(new \DateTimeImmutable);
-
-    }
+    
+    
 }
