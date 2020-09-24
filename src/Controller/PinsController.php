170193e4c5eb2062d1e1d08400fe5757041fef6c
tidjane95 <tidjane271295@gmail.com>
2020-09-24 18:34:21 +0000

    Modifier la page show

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Pin;
 use App\Form\PinType;
 use App\Repository\PinRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,12 +35,15 @@ class PinsController extends AbstractController
     /**
      * @Route("/pins/create", name="app_pins_create", methods={"GET", "POST"} )
      */
-    public function create(Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepo): Response
     {
         $pin= new Pin;
         $form= $this->createForm(PinType:: class, $pin);
         $form->handleRequest($request);
         if($form-> isSubmitted() && $form-> isValid()){
+            $sara = $userRepo->findOneBy(['email' => 'sara@example.com']);
+            $pin-> setUser($sara);
+
             $em -> persist($pin);
             $em -> flush();
             $this-> addFlash('success', 'Pin Ajouter avec succe');
